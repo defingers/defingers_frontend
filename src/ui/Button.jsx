ac81aa4d9a342ba122e26cb8a84911626adb125a
Rakesh Reddy <rakeshreddysarikonda11@gmail.com>
2025-08-22 17:32:57 +0530

    basic structure

---
+++ src/ui/Button.jsx
@@ -0,0 +1,26 @@
+
+const colorClasses = {
+  orange: "bg-orange-500 hover:bg-orange-600 text-white",
+  yellow: "bg-yellow-400 hover:bg-yellow-500 text-black",
+  gray: "bg-gray-200 hover:bg-gray-300 text-black",
+  light: "bg-yellow-300 hover:bg-yellow-400 text-white",
+  default: "bg-gray-100 hover:bg-gray-200 text-black",
+};
+
+export default function Button({
+  children,
+  color = "default",
+  className = "",
+  ...props
+}) {
+  return (
+    <button
+      className={`px-6 py-2 rounded-lg font-semibold shadow-md transition ${
+        colorClasses[color] || colorClasses.default
+      } ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
