9d99c61569625a973a18a1f1a599a34f5024cb1d
Rakesh Reddy <rakeshreddysarikonda11@gmail.com>
2025-08-24 12:49:21 +0530

    implemented boiler plate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+// import './App.css'
 import Navbar from './components/Navbar.jsx'
 
 function App() {
@@ -6,8 +6,6 @@ function App() {
   return (
     <>
       <Navbar />
-     <p className='text-2xl font-bold text-red-500'>Hello World </p>
-
     </>
   )
 }
